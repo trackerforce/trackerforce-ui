8dc45fb76c32ca85f7607c8b3ca405f080294da6
petruki <31597636+petruki@users.noreply.github.com>
2021-10-15 18:36:37 -0700

    Created Management/logged components

--- src/app/logged/logged-routing.module.ts
+++ src/app/logged/logged-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../auth/guards/auth.guard';
+import { AgentsComponent } from './agents/agents.component';
+import { GlobalsComponent } from './globals/globals.component';
 import { HomeComponent } from './home/home.component';
+import { ProceduresComponent } from './procedures/procedures.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TemplatesComponent } from './templates/templates.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard], 
+    children: [
+      { path: 'agents', component: AgentsComponent },
+      { path: 'tasks', component: TasksComponent },
+      { path: 'procedures', component: ProceduresComponent },
+      { path: 'templates', component: TemplatesComponent },
+      { path: 'globals', component: GlobalsComponent }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/logged/logged.module.ts
+++ src/app/logged/logged.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { LoggedRoutingModule } from './logged-routing.module';
 import { HomeComponent } from './home/home.component';
+import { AgentsComponent } from './agents/agents.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { ProceduresComponent } from './procedures/procedures.component';
+import { TemplatesComponent } from './templates/templates.component';
+import { GlobalsComponent } from './globals/globals.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    AgentsComponent,
+    TasksComponent,
+    ProceduresComponent,
+    TemplatesComponent,
+    GlobalsComponent
   ],
   imports: [
     CommonModule,
